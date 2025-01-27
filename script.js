5eeb1e74dc566a66f40711bbeef52b3c178eba2f
mvgui <michellyvg@yahoo.com.br>
2025-01-26 21:04:44 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//Programação é o ato de escrever instruções para que um computador execute tarefas.
+
+//JavaScript é uma linguagem de programação que torna as páginas da web interativas.
+
+/*
+A função do JavaScript em um navegador web é tornar as páginas interativas. 
+Ele permite ações como cliques em botões, animações e atualização de conteúdo sem recarregar a página. 
+Em resumo, o JavaScript transforma páginas estáticas em experiências dinâmicas e envolventes.
+*/
